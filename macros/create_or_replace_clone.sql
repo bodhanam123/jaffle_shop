a24bd15674e9ec485ef052f5e66eb13518415a70
Pranavi_Nxtwave <pranavi.b@nxtwave.co.in>
2023-09-20 07:34:48 +0000

    commit

--- macros/create_or_replace_clone.sql
+++ macros/create_or_replace_clone.sql
@@ -1,8 +1,14 @@
-{% macro create_or_replace_clone(this_relation, defer_relation) %}
-    {{ return(adapter.dispatch('create_or_replace_clone', 'dbt')(this_relation, defer_relation)) }}
+{% macro default__create_or_replace_clone() %}
+
+{% set payment_methods_query %}
+ create or replace table  {{ ref('stg_orders') }}  as {{ ref('raw_customers') }}
+{% endset %}
+
+{% set results = run_query(payment_methods_query) %}
+    
 {% endmacro %}
 
-{% macro default__create_or_replace_clone(this_relation, defer_relation) %}
-    create or replace table {{ this_relation }} clone {{ defer_relation }};
-    create or replace table `project-on-dbt-379106.team_one.test_multi_queries` clone {{ defer_relation }};
-{% endmacro %}
+
+
+
+  
