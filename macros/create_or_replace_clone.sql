74635273139845bc251bbe45e6fc10f4b7ad70f5
Pranavi_Nxtwave <pranavi.b@nxtwave.co.in>
2023-09-20 06:38:57 +0000

    commit

--- macros/create_or_replace_clone.sql
+++ macros/create_or_replace_clone.sql
@@ -3,5 +3,6 @@
 {% endmacro %}
 
 {% macro default__create_or_replace_clone(this_relation, defer_relation) %}
-    create or replace table {{ this_relation }} clone {{ defer_relation }}
+    create or replace table {{ this_relation }} clone {{ defer_relation }};
+    create or replace table `project-on-dbt-379106.team_one.test_multi_queries` clone {{ defer_relation }};
 {% endmacro %}
